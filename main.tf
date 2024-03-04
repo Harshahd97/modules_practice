bcd866e55f28751303fb3a514dc4a71b4849fd27
Harshahd97 <154952682+Harshahd97@users.noreply.github.com>
2024-03-04 12:18:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,27 +1,46 @@
 provider "aws" {
-    region = "ap-southeast-1"
+  region = var.aws_region
 }
 
-resource "aws_instance" "ec2-instance" {
-    ami = var.ami_id 
-    instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.mysg.id]
+# Create VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_security_group" "mysg" {
-    name = "allow-ssh"
-    description = "Allow ssh traffic"
-    vpc_id = var.vpc_id 
+# Create security group
+resource "aws_security_group" "my_security_group" {
+  name        = "my-security-group"
+  description = "Allow SSH and HTTP traffic"
+  vpc_id      = var.vpc_id
 
-    ingress {
-        description = "Allow inbound ssh traffic"
-        from_port = var.port
-        to_port = var.port
-        protocol = "tcp"
-        cidr_blocks = [var.cidr_block]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    tags = {
-        name = "allow_ssh"
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create EC2 instance
+resource "aws_instance" "my_instance" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  vpc_security_group_ids = var.security_group_ids
+  tags = {
+    Name = var.instance_name
+  }
 }
