25030333d56feee5497897ad4b945dd092da69dc
Harshahd97 <154952682+Harshahd97@users.noreply.github.com>
2024-03-04 12:19:15 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,4 +1,14 @@
-output "public_ip" {
-    value = aws_instance.ec2-instance.public_ip
-    description = "Public IP Address of Instance"
+output "ec2_instance_public_ip" {
+  value = aws_instance.my_instance.public_ip
+  description = "Public IP address of the EC2 instance"
+}
+
+output "ec2_instance_private_ip" {
+  value = aws_instance.my_instance.private_ip
+  description = "Private IP address of the EC2 instance"
+}
+
+output "ec2_instance_id" {
+  value = aws_instance.my_instance.id
+  description = "ID of the EC2 instance"
 }
